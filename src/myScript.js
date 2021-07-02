8d6d38f8c145382d38c89ff1ec5cf39573a0d9ce
hojun.lee <60565211+paullabkorea@users.noreply.github.com>
2021-07-02 11:12:06 +0900

    Add files via upload

---
+++ src/myScript.js
@@ -0,0 +1,40 @@
+// 테스트 시작 누르면 얼굴 인식 페이지로 이동
+let contMain = document.querySelector(".cont-main");
+let testStartBtn = document.querySelector(".btn-test-start");
+let cont = document.querySelector(".cont");
+let btn = document.querySelector(".btn-start");
+let resultTxt = document.querySelector(".title_result");
+let webcamEmpty = document.querySelector(".webcam-empty");
+let cover = document.querySelector(".cover");
+
+testStartBtn.addEventListener("click", function(){
+    contMain.style.display = "none";
+    cont.style.display = "inline-block";
+});
+
+btn.addEventListener("click", function() {
+    resultTxt.style.display = "block";
+    webcamEmpty.style.display = "none";
+    cover.style.display = "block";
+});
+
+// 메인화면 - 이미지 슬라이드
+const imgSlideItems = document.querySelectorAll(".img-slide-item");
+let firstItem = imgSlideItems[0];
+
+function imageSlide() {
+    const currentItem = document.querySelector(".img-slide-item.active");
+    if(currentItem) {
+        currentItem.classList.remove("active");
+        let next = currentItem.nextElementSibling;
+        if(next) {
+            next.classList.add("active");
+        } else {
+            firstItem.classList.add("active");
+        }
+    } else {
+        firstItem.classList.add("active");
+    }
+}
+
+setInterval(imageSlide, 200);
